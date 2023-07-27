32b393c6553de530f82b286756cc00787443b39c
Bekzod Usarov <bekzodusarov@mail.ru>
2023-07-27 10:54:53 +0500

    first push

--- lib/presentation/screens/splash_screen/splash_screen.dart
+++ lib/presentation/screens/splash_screen/splash_screen.dart
@@ -4,35 +4,49 @@ import 'package:gnexus/data/models/login_model/profile_access_model.dart';
 import 'package:gnexus/presentation/screens/bottom_navigation/bottom_navigation.dart';
 import 'package:gnexus/presentation/screens/login_screen/login_screen.dart';
 
-
 import '../../../data/apis/login_api/profile_access.dart';
 import '../../../utils/status_code/status_code.dart';
 
-class SplashScreen extends StatelessWidget {
-   SplashScreen({super.key});
+class SplashScreen extends StatefulWidget {
+  SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
   ProfileAccess? profileAccessInfo;
+
+  @override
+  void initState() {
+    navigator();
+    super.initState();
+  }
+
+  void navigator() async {
+    await Future.delayed(Duration.zero);
+    {
+      ProfileAccessRepository.getInstance().sendToken();
+      Future.delayed(Duration(seconds: 2)).then((value) {
+        Navigator.of(context).popUntil((route) => route.isFirst);
+        if (StatusCode.successStatusCode == 401) {
+          Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                  builder: (BuildContext context) => LoginScreen()));
+        } else {
+          Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                  builder: (BuildContext context) =>
+                      MainPage(selectedIndex: 0)));
+        }
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    ProfileAccessRepository.getInstance().sendToken();
-
-    Future.delayed(Duration(seconds: 2)).then((value) {
-
-      Navigator.of(context).popUntil((route) => route.isFirst);
-      if(StatusCode.successStatusCode==401){
-        Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(
-                builder: (BuildContext context) => LoginScreen()));
-      }else{
-        Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(
-                builder: (BuildContext context) => MainPage(selectedIndex: 0)));
-      }
-
-    }
-
-       );
     return Scaffold(
       body: Center(
         child: Image.asset("assets/gn_logo.png"),
